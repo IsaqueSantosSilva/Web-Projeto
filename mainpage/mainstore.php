9876dd6f5990d565e9f0b9d99dd0d6768b40de19
Hallysson Ribeiro <hallyssonribeiro2001@gmail.com>
2021-04-09 10:48:06 -0300

    Dados de login/card de compra

--- mainpage/mainstore.php
+++ mainpage/mainstore.php
@@ -1,15 +1,47 @@
+<?php 
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Loja Principal</title>
 	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
+	<link rel="stylesheet" type="text/css" href="../css/mainstore.css">
 </head>
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
-<body>
-<h1>Loja principal</h1>
-<h2>Bem vindo</h2>
 
-	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"></script>
-</body>
-</html>
+<body>
+	<div class="login-data">
+		<label>
+			<?php echo "Usuário:" ."<i class='user icon'></i>"  . "<strong>" . $_SESSION['user'] . "</strong>" ?>
+		</label>
+	</div>
+
+	<h1><i>NOME LOJA</i></h1>
+	<section>
+		<div class="ui cards">
+			<div class="card">
+				<div class="ui image">
+					<img src="big.jpg">
+				</div>
+				<div class="content">
+					<div class="header">Big Smoke</div>
+					<div class="description">
+						I'll have two number 9s, a number 9 large, a number 6 with extra dip, a number 7, two number 45s, one with cheese, and a large soda.
+					</div>
+				</div>
+				<div class="ui vertical animated button" tabindex="0">
+					<div class="hidden content">Shop</div>
+					<div class="visible content">
+						<i class="shop icon"></i>
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"></script>
+	</body>
+	</html>
--- register/index.php
+++ register/index.php
@@ -10,27 +10,29 @@ if (isset($_POST['btn'])) {
 <head>
 	<title>Loja</title>
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
 </head>
 <body>
 	<form class="ui form" method="POST" action=" <? $_SERVER['PHP_SELF'] ?>">
+		<h2>Cadastro</h2>
 		<div class="field">
 			<label>Nome de Usuário</label>
-			<input type="text" name="username" placeholder="">
+			<input type="text" name="username" required>
 		</div>
 		<div class="field">
 			<label>Idade</label>
-			<input type="text" name="age" placeholder="">
+			<input type="text" name="age" required>
 		</div>
 		<div class="field">
 			<label>Senha</label>
-			<input type="password" name="pwd" placeholder="">
+			<input type="password" name="pwd" required>
 		</div>
 		<div class="field">
 			<label>Confirmar Senha</label>
-			<input type="password" name="cpwd" placeholder="">
+			<input type="password" name="cpwd" required>
 		</div>
 		
-		<button class="ui button" name="btn" type="submit">Enviar</button>
+		<button class="ui black button" name="btn" type="submit">Enviar</button>
 	</form>
 
 
