3a267d5a0b7bbee103e34662d441d117e9d2a0b4
Hallysson Ribeiro <hallyssonribeiro2001@gmail.com>
2021-04-19 11:41:35 -0300

    Modal added when i press a button to buy something

--- js/script.js
+++ js/script.js
@@ -1,2 +1,146 @@
 // Codigo para DROPDOWN
 $('.ui.dropdown').dropdown();
+
+function callHorizon(){
+	$.sweetModal.confirm('Deseja comprar Horizon Zero Dawn?', 'Confirme?', function() {
+		$.sweetModal('Obrigado por comprar <i class = "check icon"></i>');
+	}, function() {
+		$.sweetModal('Você cancelou esta compra');
+	});
+}
+
+function callLife(){
+	$.sweetModal.confirm('Deseja comprar Life is Strange?', 'Confirme?', function() {
+		$.sweetModal('Obrigado por comprar <i class = "check icon"></i>');
+	}, function() {
+		$.sweetModal('Você cancelou esta compra');
+	});
+}
+
+function callColossus(){
+	$.sweetModal.confirm('Deseja comprar Shadow of the Colossus?', 'Confirme?', function() {
+		$.sweetModal('Obrigado por comprar <i class = "check icon"></i>');
+	}, function() {
+		$.sweetModal('Você cancelou esta compra');
+	});
+}
+
+function callWitch(){
+	$.sweetModal.confirm('Deseja comprar Blair Witch?', 'Confirme?', function() {
+		$.sweetModal('Obrigado por comprar <i class = "check icon"></i>');
+	}, function() {
+		$.sweetModal('Você cancelou esta compra');
+	});
+}
+
+function callDeadSpace(){
+	$.sweetModal.confirm('Deseja comprar Dead Space 3?', 'Confirme?', function() {
+		$.sweetModal('Obrigado por comprar <i class = "check icon"></i>');
+	}, function() {
+		$.sweetModal('Você cancelou esta compra');
+	});
+}
+
+function callRE(){
+	$.sweetModal.confirm('Deseja comprar Resident Evil 7?', 'Confirme?', function() {
+		$.sweetModal('Obrigado por comprar <i class = "check icon"></i>');
+	}, function() {
+		$.sweetModal('Você cancelou esta compra');
+	});
+}
+
+function callTetris(){
+	$.sweetModal.confirm('Deseja comprar Tetris?', 'Confirme?', function() {
+		$.sweetModal('Obrigado por comprar <i class = "check icon"></i>');
+	}, function() {
+		$.sweetModal('Você cancelou esta compra');
+	});
+}
+
+function callTangram(){
+	$.sweetModal.confirm('Deseja comprar Tangram?', 'Confirme?', function() {
+		$.sweetModal('Obrigado por comprar <i class = "check icon"></i>');
+	}, function() {
+		$.sweetModal('Você cancelou esta compra');
+	});
+}
+
+function callRoom(){
+	$.sweetModal.confirm('Deseja comprar The Room?', 'Confirme?', function() {
+		$.sweetModal('Obrigado por comprar <i class = "check icon"></i>');
+	}, function() {
+		$.sweetModal('Você cancelou esta compra');
+	});
+}
+
+function callNeedSpeed(){
+	$.sweetModal.confirm('Deseja comprar Need for Speed?', 'Confirme?', function() {
+		$.sweetModal('Obrigado por comprar <i class = "check icon"></i>');
+	}, function() {
+		$.sweetModal('Você cancelou esta compra');
+	});
+}
+
+function callAsphalt(){
+	$.sweetModal.confirm('Deseja comprar Asphalt 8: Airborne?', 'Confirme?', function() {
+		$.sweetModal('Obrigado por comprar <i class = "check icon"></i>');
+	}, function() {
+		$.sweetModal('Você cancelou esta compra');
+	});
+}
+
+function callCrew(){
+	$.sweetModal.confirm('Deseja comprar The Crew 2?', 'Confirme?', function() {
+		$.sweetModal('Obrigado por comprar <i class = "check icon"></i>');
+	}, function() {
+		$.sweetModal('Você cancelou esta compra');
+	});
+}
+
+function callFifa(){
+	$.sweetModal.confirm('Deseja comprar Fifa 16?', 'Confirme?', function() {
+		$.sweetModal('Obrigado por comprar <i class = "check icon"></i>');
+	}, function() {
+		$.sweetModal('Você cancelou esta compra');
+	});
+}
+
+function callPES(){
+	$.sweetModal.confirm('Deseja comprar PES2019?', 'Confirme?', function() {
+		$.sweetModal('Obrigado por comprar <i class = "check icon"></i>');
+	}, function() {
+		$.sweetModal('Você cancelou esta compra');
+	});
+}
+
+function callRocket(){
+	$.sweetModal.confirm('Deseja comprar Rocket League?', 'Confirme?', function() {
+		$.sweetModal('Obrigado por comprar <i class = "check icon"></i>');
+	}, function() {
+		$.sweetModal('Você cancelou esta compra');
+	});
+}
+
+function callFlight(){
+	$.sweetModal.confirm('Deseja comprar Microsoft Flight Simulator?', 'Confirme?', function() {
+		$.sweetModal('Obrigado por comprar <i class = "check icon"></i>');
+	}, function() {
+		$.sweetModal('Você cancelou esta compra');
+	});
+}
+
+function callArma(){
+	$.sweetModal.confirm('Deseja comprar Arma 3?', 'Confirme?', function() {
+		$.sweetModal('Obrigado por comprar <i class = "check icon"></i>');
+	}, function() {
+		$.sweetModal('Você cancelou esta compra');
+	});
+}
+
+function callSkylines(){
+	$.sweetModal.confirm('Deseja comprar Cities: Skylines?', 'Confirme?', function() {
+		$.sweetModal('Obrigado por comprar <i class = "check icon"></i>');
+	}, function() {
+		$.sweetModal('Você cancelou esta compra');
+	});
+}
